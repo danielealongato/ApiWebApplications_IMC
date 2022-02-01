de51063b045c0d7e08c3716dcf138fcf1db2fee2
Daniele Longato <43710727+danielealongato@users.noreply.github.com>
2022-01-31 22:21:12 -0300

    Included Javascript code and adjusts html

--- calculoImc.js
+++ calculoImc.js
@@ -5,6 +5,8 @@ const calculate = document.getElementById("calcular");
             var peso = document.getElementById("peso").value;
             var altura = document.getElementById("altura").value;
             var res = document.getElementById("res");
+            var description = document.getElementById("description")
+            var resumo = document.getElementById("abstract")
 
 
             if (altura !== '' && peso !== ''){
@@ -13,12 +15,25 @@ const calculate = document.getElementById("calcular");
 
 
                 let tabelaComparacao = '';
+                let resumo = '';
 
                 if (imc < 18.5){
-                    tabelaComparacao = 'Abaixo do Peso.'
+                    resumo = 'Seu peso está: Abaixo do Normal'
+                    tabelaComparacao = 'Nossa como vc está Magrissimo!!'
+                }else if (imc < 24.9){
+                    resumo = 'Seu peso está: Normal'
+                    tabelaComparacao = 'Mandou bem hein, está em forma!'
+                }else if (imc < 30){
+                    resumo = 'Seu peso está: Acima do Normal'
+                    tabelaComparacao = 'Humm acho que vamos precisar cortar o docinho!'
+                }else {
+                    resumo = 'Seu peso está: Muito acima do Normal'
+                    tabelaComparacao = 'Acho que passamos do ponto, melhor procurar um médico...  '
                 }
 
-                res.textContent = `Seu IMC é ${imc} e você está ${tabelaComparacao}`;
+                res.textContent = `Seu IMC é: ${imc}`;
+                description.textContent = `${tabelaComparacao}`;
+                abstract.textContent = `${resumo}`;
 
 
 
